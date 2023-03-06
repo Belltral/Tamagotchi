631400eadf892c6e6fbacdf87d71046b1ae083ad
Henrique Reis <henriquereis198@outlook.com>
2023-03-06 18:38:24 -0300

    Primeira implementação - consumindo a API e retornando o resultado em JSON

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cria a requisição GET do link da API
+            var client = new RestClient("https://pokeapi.co/api/v2/pokemon/");
+            var request = new RestRequest("", Method.Get);
+            var response = client.Execute(request);
+
+            // Converte o resultado da API para o formato JSON legível
+            JToken token = JToken.Parse(response.Content);
+            string formattedJson = token.ToString(Formatting.Indented);
+            Console.WriteLine(formattedJson);
+        }
+    }
+}
